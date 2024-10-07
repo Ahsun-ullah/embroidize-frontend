fcf243086fedff30273ca5a571064696c0aa7081
ahsun-ullah <ahsunrahim@gmail.com>
2024-10-07 20:47:05 +0600

    redux setup done

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,22 +1,24 @@
 import { Inter } from "next/font/google";
-import Head from "next/head";
 import "remixicon/fonts/remixicon.css";
 import "./globals.css";
 import { Providers } from "./providers";
+import StoreProvider from "./StoreProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <title>EmbroID</title>
-      <Head>
+      <head>
+        <title>EmbroID</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+      </head>
       <body className={inter.className}>
-        <Providers>
-          <main className="min-h-screen">{children}</main>
-        </Providers>
+        <StoreProvider>
+          <Providers>
+            <main className="min-h-screen">{children}</main>
+          </Providers>
+        </StoreProvider>
       </body>
     </html>
   );
