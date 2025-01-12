ce4022f820ba2dc9d20f29b619ab140f4515b0ae
ahsun-ullah <ahsunrahim@gmail.com>
2025-01-12 19:11:36 +0600

    admin dashboard layout fixed

--- src/components/admin/AdminSidebar.jsx
+++ src/components/admin/AdminSidebar.jsx
@@ -1,20 +1,33 @@
 'use client';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function AdminSidebar({ isCollapsed, setIsCollapsed }) {
   return (
     <div className={`admin-sidebar ${isCollapsed ? 'collapsed' : ''}`}>
-      <button
-        onClick={() => setIsCollapsed(!isCollapsed)}
-        className="collapse-btn"
-      >
-        {isCollapsed ? '→' : '←'}
-      </button>
+      <div className="flex items-center justify-between mb-8">
+        {!isCollapsed ? (
+          <Link href={'/admin'} className="sidebar-text">
+            <Image src={'/twitter-bird-logo-pictures-0.png'} alt='logo' width={50} height={50} />
+          </Link>
+        ) : (
+          '*'
+        )}
+        <button
+          onClick={() => setIsCollapsed(!isCollapsed)}
+          className="collapse-btn"
+        >
+          {isCollapsed ? '→' : '←'}
+        </button>
+      </div>
       <ul>
         <li>
           <Link href="/admin">
-            {' '}
-            *{!isCollapsed && <span className="sidebar-text">Dashboard</span>}
+            {!isCollapsed ? (
+              <span className="sidebar-text">Dashboard</span>
+            ) : (
+              '*'
+            )}
           </Link>
         </li>
         <li>
