aa75f8dd496b77feef4b0eca310b4f1ada7b2456
ahsun-ullah <ahsunrahim@gmail.com>
2025-04-06 12:32:08 +0600

    home page banner fixed

--- src/components/user/HomePage/HeroSection.jsx
+++ src/components/user/HomePage/HeroSection.jsx
@@ -1,38 +1,27 @@
-import { Input } from '@heroui/react';
-
 const HeroSection = () => {
   return (
-    <>
-      <section className="bg-blue-50 py-[10rem] text-black">
-        <div className="container mx-auto px-4">
-          <div className="flex flex-col items-center justify-center">
-            <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4 text-center">
-              Machine Embroidery Designs
-            </h1>
-            <p className="text-sm sm:text-base md:text-lg text-center mb-4">
-              All you need for your next machine embroidery project. The highest
-              quality for the lowest price.
-            </p>
-            <div className="flex justify-center content-center mt-4">
-              <Input
-                radius="full"
-                isBordered
-                classNames={{
-                  base: 'max-w-[25rem] max-sm:max-w-[10rem] h-10 ',
-                  mainWrapper: 'h-full',
-                  input: 'text-small',
-                  inputWrapper: 'h-full font-normal text-black bg-white ',
-                }}
-                placeholder="Type to search..."
-                size="sm"
-                endContent={<i className="ri-search-eye-fill text-xl"></i>}
-                type="search"
-              />
-            </div>
-          </div>
+    <section
+      style={{
+        backgroundImage: `url('/home-banner.jpg')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
+      className='flex items-center min-h-[470px] w-full max-w-full mx-0 px-0 bg-gray-200 relative '
+    >
+      <div className='flex justify-end absolute top-0 bottom-0 w-full pe-[10%]'>
+        <div className='flex flex-col items-center justify-center text-white'>
+          <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-4 text-center'>
+            Machine <br />
+            Embroidery Designs
+          </h1>
+          <p className='text-sm sm:text-base md:text-lg text-center mb-4 max-w-2xl'>
+            All you need for your next machine embroidery project. <br /> The
+            highest quality for the lowest price.
+          </p>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
