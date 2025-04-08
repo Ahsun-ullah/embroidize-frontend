a757aef1e14f8424cd7da4c878e4ed8204b67b44
ahsun-ullah <ahsunrahim@gmail.com>
2025-04-08 23:22:34 +0600

    login and register issues fixed

---
+++ src/utils/functions/FetchUserInfoFromServer.jsx
@@ -0,0 +1,35 @@
+import { cookies } from 'next/headers';
+
+export const fetchUserInfo = async () => {
+  const cookieStore = cookies();
+  const token = cookieStore.get('token');
+
+  if (!token?.value) {
+    console.error('Token is missing');
+    return { error: 'Token is missing' };
+  }
+
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_API_URL_PROD}/userInfo`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token.value}`,
+        },
+      },
+    );
+
+    if (!res.ok) {
+      console.error('Failed to fetch user info', res.statusText);
+      return { error: res.statusText };
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching user info:', error);
+    return { error: error.message };
+  }
+};
