24ee41c0c2ebf45a5f651087aca2207d0dbe85d8
ahsun-ullah <ahsunrahim@gmail.com>
2025-04-12 13:49:51 +0600

    all product api integration done

--- src/app/admin/all-products/page.jsx
+++ src/app/admin/all-products/page.jsx
@@ -1,7 +1,37 @@
-import { getProducts } from '@/app/apis/public/products/page';
 import LoadingSpinner from '@/components/Common/LoadingSpinner';
 import ProductsTableWrapper from '@/features/products/components/ProductsTableWrapper';
+import { cookies } from 'next/headers';
 import { Suspense } from 'react';
+
+export async function getProducts() {
+  try {
+    const cookieStore = cookies();
+    const token = cookieStore.get('token')?.value;
+
+    const headers = new Headers();
+
+    if (token) {
+      headers.set('Authorization', `Bearer ${token}`);
+    }
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_API_URL_PROD}/public/product`,
+      {
+        headers,
+        cache: 'no-store',
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error(`API request failed with status ${response.status}`);
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    throw error;
+  }
+}
 
 export default async function AllProductsListPage() {
   const allProducts = await getProducts();
