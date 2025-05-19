438a72a2ebfc2f9b47607a901336e5fe7515e05b
ahsun-ullah <ahsunrahim@gmail.com>
2025-05-19 19:55:34 +0600

    added google analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import StoreProvider from '@/lib/providers/StoreProvider';
 import UiProvider from '@/lib/providers/UiProvider';
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
 import 'remixicon/fonts/remixicon.css';
 import './globals.css';
 import { metadata } from './page';
@@ -10,14 +11,33 @@ const inter = Inter({ subsets: ['latin'], display: 'swap' });
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <head>
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src='https://www.googletagmanager.com/gtag/js?id=G-BJ81WDRVP5'
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BJ81WDRVP5');
+          `,
+          }}
+        />
+
+        {/* Domain Verification */}
         <meta
           name='p:domain_verify'
           content='a417c3036823eb607157878ef76fc2b0'
         />
-        <title>{'Free Machine Embroidery Designs - Embroidize'}</title>
-        {/* <meta name='description' content={metadata.description} /> */}
-        {/* <meta name='keywords' content={metadata.keywords.join(', ')} /> */}
+
+        {/* SEO Meta Tags */}
+        <title>Free Machine Embroidery Designs - Embroidize</title>
+
+        {/* Open Graph */}
         <meta property='og:title' content={metadata.openGraph.title} />
         <meta
           property='og:description'
@@ -25,8 +45,10 @@ export default function RootLayout({ children }) {
         />
         <meta property='og:url' content={metadata.openGraph.url} />
         <meta property='og:site_name' content={metadata.openGraph.siteName} />
-        <meta property='og:image' content={metadata.openGraph.images[0].url} />
+        <meta property='og:image' content={metadata.openGraph.images[0]?.url} />
         <meta property='og:type' content={metadata.openGraph.type} />
+
+        {/* Twitter Cards */}
         <meta name='twitter:card' content={metadata.twitter.card} />
         <meta name='twitter:title' content={metadata.twitter.title} />
         <meta
@@ -34,19 +56,12 @@ export default function RootLayout({ children }) {
           content={metadata.twitter.description}
         />
         <meta name='twitter:image' content={metadata.twitter.images[0]} />
-      </head>
+      </Head>
       <body className={inter.className} suppressHydrationWarning>
         <StoreProvider>
-          {/* <AuthProvider
-            protectedRoutes={['/admin']}
-            exactProtectedRoutes={[]}
-            loginPath='/auth/login'
-            defaultRedirect='/'
-          > */}
           <UiProvider>
             <main className='min-h-screen'>{children}</main>
           </UiProvider>
-          {/* </AuthProvider> */}
         </StoreProvider>
       </body>
     </html>
