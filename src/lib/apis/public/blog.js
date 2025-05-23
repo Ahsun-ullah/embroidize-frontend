95a254e8eaa4a11181c02bac5a5826a81f104bff
ahsun-ullah <ahsunrahim@gmail.com>
2025-05-24 01:19:38 +0600

    push

--- src/lib/apis/public/blog.js
+++ src/lib/apis/public/blog.js
@@ -9,7 +9,7 @@ export async function getBlogs(searchQuery, currentPage, perPageData) {
     if (perPageData) queryParams.append('limit', perPageData.toString());
 
     const apiUrl = `${process.env.NEXT_PUBLIC_BASE_API_URL_PROD}/public/blog?${queryParams.toString()}`;
-    console.log(apiUrl);
+
     const response = await fetch(apiUrl, {
       headers,
       cache: 'no-store',
@@ -23,10 +23,39 @@ export async function getBlogs(searchQuery, currentPage, perPageData) {
 
     return {
       blogs: result.data.blogs,
-      pagination: result.data.pagination,
+      totalCount: result.data.pagination.totalItems,
+      page: result.data.pagination.currentPage,
+      totalPages: result.data.pagination.totalPages,
     };
   } catch (error) {
     console.error('Error fetching blogs:', error);
+    throw error;
+  }
+}
+
+export async function getSingleBlog(slug) {
+  try {
+    const headers = new Headers();
+    headers.set('Authorization', 'Bearer some-static-token');
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_API_URL_PROD}/public/blog/${slug}`,
+      {
+        headers,
+        cache: 'no-store',
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error(`API request failed with status ${response.status}`);
+    }
+
+    const result = await response.json();
+    return {
+      blogs: result.data,
+    };
+  } catch (error) {
+    console.error('Error fetching product:', error);
     throw error;
   }
 }
