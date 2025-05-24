18d4c6f26b0a2a7dba336781f1ff0bdadafcdd6a
ahsun-ullah <ahsunrahim@gmail.com>
2025-05-24 21:25:57 +0600

    fix schema markup for product page

--- src/app/product/[slug]/page.jsx
+++ src/app/product/[slug]/page.jsx
@@ -7,7 +7,8 @@ import {
   getSingleProduct,
 } from '@/lib/apis/public/products';
 import { getAllProductsBySubCategory } from '@/lib/apis/public/subcategory';
-import { use } from 'react';
+import Head from 'next/head';
+import { notFound } from 'next/navigation';
 
 export async function generateMetadata({ params }) {
   try {
@@ -50,45 +51,50 @@ export async function generateMetadata({ params }) {
   }
 }
 
-export default function ProductDetails({ params }) {
-  const { slug } = use(params);
+export default async function ProductDetails({ params }) {
+  const slug = params.slug;
 
-  const response = use(getSingleProduct(slug));
+  const response = await getSingleProduct(slug);
   const product = response?.data;
 
   if (!product) return notFound();
 
   const hasSubCategory = !!product?.sub_category?.slug;
 
-  const productListResponse = use(
-    hasSubCategory
-      ? getAllProductsBySubCategory(product.sub_category.slug, 1, 6)
-      : getAllProductsByCategory(product.category?.slug, 1, 6),
-  );
+  const productListResponse = hasSubCategory
+    ? await getAllProductsBySubCategory(product.sub_category.slug, 1, 6)
+    : await getAllProductsByCategory(product.category?.slug, 1, 6);
 
   const allProducts = productListResponse?.products ?? [];
-  const popularProducts = use(getPopularProducts('', 1, 4))?.products ?? [];
+  const popularProducts = (await getPopularProducts('', 1, 4))?.products ?? [];
+
+  const schema = {
+    '@context': 'https://schema.org/',
+    '@type': 'Product',
+    name: product.name,
+    image: [product.image?.url],
+    description: product.meta_description,
+    brand: {
+      '@type': 'Brand',
+      name: product.brand || 'Embroidize',
+    },
+    offers: {
+      '@type': 'Offer',
+      url: `https://embroidize.com/product/${product.slug}`,
+      priceCurrency: 'USD',
+      price: product.price === 0 ? 'Free' : product.price.toFixed(2),
+      availability: 'https://schema.org/InStock',
+    },
+  };
+
   return (
     <>
-      <script
-        type='application/ld+json'
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            '@context': 'https://schema.org/',
-            '@type': 'Product',
-            name: product.name,
-            image: [product.image?.url],
-            description: product.meta_description || product.description,
-            offers: {
-              '@type': 'Offer',
-              url: `https://embroidize.com/product/${product.slug}`,
-              priceCurrency: 'USD',
-              price: product.price,
-              availability: 'InStock',
-            },
-          }),
-        }}
-      />
+      <Head>
+        <script
+          type='application/ld+json'
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+        />
+      </Head>
 
       <Header />
       <SingleProductComponent
