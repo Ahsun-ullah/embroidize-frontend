eceb439b0cf32e7607e1bbc540e40bdaccef6d59
ahsun-ullah <ahsunrahim@gmail.com>
2025-10-19 10:13:13 +0600

    add twakto

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -44,14 +44,14 @@ export default function RootLayout({ children }) {
         dangerouslySetInnerHTML={{
           __html: `
         var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-        (function(){
-          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-          s1.async=true;
-          s1.src='https://embed.tawk.to/68d4e2b7d256dc1950c32019/1j5vpb81o';
-          s1.charset='UTF-8';
-          s1.setAttribute('crossorigin','*');
-          s0.parentNode.insertBefore(s1,s0);
-        })();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/68d4e2b7d256dc1950c32019/1j7lv3h9q';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
       `,
         }}
       />
