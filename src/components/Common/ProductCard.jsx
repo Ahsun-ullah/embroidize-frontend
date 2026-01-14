80d78866931c5e93f5bb6af85b761f7c66c0eea7
ahsun-ullah <ahsunrahim@gmail.com>
2026-01-14 18:55:28 +0600

    fix some frontend fixing

--- src/components/Common/ProductCard.jsx
+++ src/components/Common/ProductCard.jsx
@@ -59,7 +59,7 @@ const ProductCard = React.memo(function ProductCard({ item, index = 0 }) {
         <SkuFlag sku={item.sku_code} />
       )}
       {/* Admin Choice Star */}
-      {item?.sku_code && userInfoData?.role === 'admin' && (
+      {item?.isAdminChoice && userInfoData?.role === 'admin' && (
         <div className='absolute top-[0.1 px] left-2 z-10'>
           <AdminChoiceStar status={item.isAdminChoice} />
         </div>
