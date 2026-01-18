128f87537dacf7df7b508da113ce568bbd3215c0
ahsun-ullah <ahsunrahim@gmail.com>
2026-01-18 14:20:17 +0600

    push

--- next.config.js
+++ next.config.js
@@ -16,16 +16,11 @@ const nextConfig = {
   compress: true,
 
   images: {
-    domains: [
-      'www.embroidize.com',
-      'embroidize-assets.nyc3.cdn.digitaloceanspaces.com',
-    ],
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
-      { protocol: 'https', hostname: 'embroidize.com', pathname: '/**' },
       {
         protocol: 'https',
-        hostname: 'tan-dogfish-413907.hostingersite.com',
+        hostname: 'www.embroidize.com',
         pathname: '/**',
       },
       {
@@ -35,10 +30,24 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'embroidize.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tan-dogfish-413907.hostingersite.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'embroidize-assets.nyc3.digitaloceanspaces.com',
         pathname: '/**',
       },
-      { protocol: 'https', hostname: 'res.cloudinary.com', pathname: '/**' },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
     ],
   },
 
